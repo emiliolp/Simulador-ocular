518682300dd17c26a51bf393c5029ce129a3d3fb
Emilio López <elp10.el@gmail.com>
2017-07-24 22:35:04 +0200

    Update HelpDialog.cpp

--- EmilioLópez-ArucoTest/HelpDialog.cpp
+++ EmilioLópez-ArucoTest/HelpDialog.cpp
@@ -6,10 +6,9 @@ using namespace aruco_test;
 int HelpDialog::showCalibrate(){
 
     winCalib = new CalibWindow((Fl::w()/2)-390,(Fl::h()/2)-200,780,400,"Instrucciones de calibración");
-    ///win_calib = new WinCalibrate((Fl::w()/2)-300,(Fl::h()/2)-225,600,450,"Instrucciones de calibración");
     winCalib->resizable(pop);
     winCalib->color(Fl_Color(7));
-                      // initialize image lib
+
     boxCalibra1 = new Fl_Box(110,0,14,500);     // widget that will contain image
     boxCalibra2 = new Fl_Box(290,0,14,500);
     boxCalibra3 = new Fl_Box(470,0,14,500);
@@ -29,8 +28,8 @@ int HelpDialog::showCalibrate(){
     calibImage3->inactive();
     calibImage4->inactive();
 
-    calib_p2d.clear();
-    calib_p3d.clear();
+    calibP2d.clear();
+    calibP3d.clear();
 
     winCalib->show();
 
@@ -38,15 +37,35 @@ int HelpDialog::showCalibrate(){
 
 }
 
-    void HelpDialog::Close(Fl_Widget*w, void *d){
+    void HelpDialog::close(Fl_Widget*w, void *d){
+
         pop->hide();
         popupActive = false;
         flagCalibration = false;
-        if (!TheCameraParams.isValid())
+        if (!cameraParams.isValid())
             fl_message("Es necesario realizar las tareas de calibración\nantes de empezar a utilizar el simulador","Aceptar",0,0);
     }
 
-    void HelpDialog::ShowMarker(Fl_Widget*w, void *d){
+    void HelpDialog::showMarker(Fl_Widget*w, void *d){
+
+        char buffer[MAX_PATH];
+        char *tag = NULL;
+        std::string path;
+        GetModuleFileName( NULL, buffer, MAX_PATH );
+        string::size_type pos = string( buffer ).find_last_of( "\\/" );
+        path = "rundll32.exe C:\\WINDOWS\\system32\\shimgvw.dll,ImageView_Fullscreen " + string( buffer ).substr( 0, pos) + "\\marcador.jpg";
+        char *cstr = new char[path.length() + 1];
+        strcpy(cstr, path.c_str());
+        WinExec(cstr, SW_SHOW);
+    }
+
+    void HelpDialog::callCalibrate(Fl_Widget*w, void *d){
+
+        flagCalibration = true;
+        showCalibrate();
+
+    }
+
         char buffer[MAX_PATH];
         char *tag = NULL;
         std::string path;
