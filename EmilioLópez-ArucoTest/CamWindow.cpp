a0c089835a37583ad0f7bd79596d9b823e51605a
Emilio López <elp10.el@gmail.com>
2017-07-24 22:33:27 +0200

    Update CamWindow.cpp

--- EmilioLópez-ArucoTest/CamWindow.cpp
+++ EmilioLópez-ArucoTest/CamWindow.cpp
@@ -4,49 +4,288 @@
 using namespace aruco_test;
 
 void CamWindow::vIdle()
-    {
+{
+    videoCapturer.grab();
+    videoCapturer.retrieve( inputImage);
 
-        if (TheCaptureFlag) {
+    //Validamos que se haya capturado una imagen de la cámara
+    if(!inputImage.empty()){
 
-            TheVideoCapturer.grab();
-            TheVideoCapturer.retrieve( TheInputImage);
+        TheUndInputImage.create(inputImage.size(),CV_8UC3);
+        cv::cvtColor(inputImage,inputImage,CV_BGR2RGB);
 
-            //Validamos que se haya capturado una imagen de la cámara
-            if(!TheInputImage.empty()){
+        MarkerDetector::Params params;
+        //play with this paramteres if the detection does not work correctly
+        params._borderDistThres=.01;//acept markers near the borders
+        params._thresParam1=5;
+        params._thresParam1_range=5;//search in wide range of values for param1
+        params._cornerMethod=MarkerDetector::SUBPIX;//use subpixel corner refinement
+        params._subpix_wsize= (15./2000.)*float(inputImage.cols) ;//search corner subpix in a 5x5 widow area
+        PPDetector.setParams(params);//set the params above
+        PPDetector.setDictionary("ARUCO_MIP_36h12");
 
-                TheUndInputImage.create(TheInputImage.size(),CV_8UC3);
-                cv::cvtColor(TheInputImage,TheInputImage,CV_BGR2RGB);
+        TheMarkers=PPDetector.detect(inputImage);
 
-                MarkerDetector::Params params;
-                //play with this paramteres if the detection does not work correctly
-                params._borderDistThres=.01;//acept markers near the borders
-                params._thresParam1=5;
-                params._thresParam1_range=5;//search in wide range of values for param1
-                params._cornerMethod=MarkerDetector::SUBPIX;//use subpixel corner refinement
-                params._subpix_wsize= (15./2000.)*float(TheInputImage.cols) ;//search corner subpix in a 5x5 widow area
-                PPDetector.setParams(params);//set the params above
-                PPDetector.setDictionary("ARUCO_MIP_36h12");
-                ///PPDetector.setDictionary("ARUCO");
+        cv::resize(inputImage,TheResizedImage,TheGlWindowSize);
+    }
+    ///else
+       /// fl_message("Error al tomar la imagen de la cámara");
 
-                TheMarkers=PPDetector.detect(TheInputImage);
-                //vector<Point2f> corners;
-                //vector<int> ids;
+}
 
-                cv::resize(TheInputImage,TheResizedImage,TheGlWindowSize);
-                strcpy(buffer,cadena);
+//Método para seleccionar un tag
+void CamWindow::detectMarker(){
 
+    Point2f maux;
+    float v[50], menor;
+    int indice;
+    Point2f TheMarkerAux;
+    Point2f MarkerAnt, PointMarkerAnt;
+
+    //Si se han encontrado tags
+    if(TheMarkers.size() != 0){
+
+        PointMarkerAnt = markerChoosed.getCenter();
+
+        if(isnan(PointMarkerAnt.x)){                        //Si es el primer tag detectado en la ejecución
+
+            if(TheMarkers.size() == 1){                     //Si sólo hay un tag de tectado lo seleccionamos
+
+                markerChoosed = TheMarkers[0];
+                TheMarkerAux = markerChoosed.getCenter();
+
+            }
+            else{                                           //En caso contrario, mostramos un error
+
+                fl_message("Error: varios tags detectados");
+
+            }
+        }
+        else{                                               //Si no se trata del primer tag
+
+            if(TheMarkers.size() == 1){
+
+                markerChoosed = TheMarkers[0];                //Si sólo hay un tag de tectado lo seleccionamos
+                econt = 0;
+
+            }
+
+            else if(TheMarkers.size() > 1){                 //Si encontramos varios tags
+
+                econt++;
+
+                if(econt >= 20){                            //Durante 5 segundos el número de tags es mayor que 1
+
+                    fl_message("Error: varios tags detectados");
+
+                }
+
+                else{
+
+                    for (unsigned int m=0;m<TheMarkers.size();m++){         //Seleccionamos el tag más cercano al anterior detectado
+
+                        TheMarkerAux = TheMarkers[m].getCenter();
+                        maux.x = PointMarkerAnt.x - TheMarkerAux.x;
+                        maux.y = PointMarkerAnt.y - TheMarkerAux.y;
+                        v[m] = maux.x + maux.y;
+
+                    }
+
+                    menor = v[0];
+                    indice = 0;
+
+                    for(unsigned int m=0; m<TheMarkers.size(); m++){
+
+                        if(v[m] < menor){
+                            menor = v[m];
+                            indice = m;
+                        }
+                    }
+
+                    markerChoosed = TheMarkers[indice];
+
+                }
             }
         }
     }
 
-    //Método para seleccionar un tag
-    void CamWindow::detectMarker(){
+    else
+        econt = 0;
 
-        Point2f maux;
-        float v[50], menor;
-        int indice;
-        Point2f TheMarkerAux;
-        Point2f MarkerAnt, PointMarkerAnt;
+    //Si el marcador 1 se ha asignado calculamos
+    if ( markerChoosed.empty() == 0 && cameraParams.isValid() )
+        markerChoosed.calculateExtrinsics(0.04,cameraParams);
+
+}
+
+
+
+void CamWindow::draw() {
+
+        //Capturamos la imagen de la cámara y detectamos los marcadores disponibles
+        vIdle();
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+        //Dibuja la imagen en el visor
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0, TheGlWindowSize.width, 0, TheGlWindowSize.height, -1.0, 1.0);
+        glViewport(0, 0, TheGlWindowSize.width , TheGlWindowSize.height);
+        glDisable(GL_TEXTURE_2D);
+        glPixelZoom( 1, -1);
+        glRasterPos3f( 0, TheGlWindowSize.height  - 0.5, -1.0 );
+        glDrawPixels ( TheGlWindowSize.width , TheGlWindowSize.height , GL_RGB , GL_UNSIGNED_BYTE , TheResizedImage.ptr(0) );
+
+        double proj_matrix[16];
+        proj_matrix[0] = 2;
+        proj_matrix[1] = -0;
+        proj_matrix[2] = 0;
+        proj_matrix[3] = 0;
+        proj_matrix[4] = 0;
+        proj_matrix[5] = -3.1;
+        proj_matrix[6] = 0;
+        proj_matrix[7] = 0;
+        proj_matrix[8] = 0.1;
+        proj_matrix[9] = 0.1;
+        proj_matrix[10] = -1;
+        proj_matrix[11] = -1;
+        proj_matrix[12] = 0;
+        proj_matrix[13] = -0;
+        proj_matrix[14] = -0.1;
+        proj_matrix[15] = 0;
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glLoadMatrixd(proj_matrix);
+
+        //Seleccionamos únicamente un tag en el caso de que la cámara detecte varios
+        detectMarker();
+
+        //Almacenamos la posición (x,y) del tag detectado
+        Point2f MarkerAux = markerChoosed.getCenter();
+        Point3D MarkerPx, MarkerCm;
+        MarkerPx.readPoint3D(MarkerAux.x,MarkerAux.y,0);
+        if (not isnan(MarkerAux.x)){
+            MarkerPx.translateCentre();
+            MarkerCm.setX(((MarkerPx.getX() * 0.65)/320) + 0.06);
+            MarkerCm.setY(((MarkerPx.getY() * 0.42)/240) - 0.38);
+        }
+
+        //Sólo indicamos el indicador del tag cuando éste cambia de posición
+        if( MarkerCm.getX() != markerAnt.x && MarkerCm.getY() != markerAnt.y){
+
+            glTranslatef(0.0f,0.0f,-1.3f);
+
+            glColor3f(1,0.0,0.0);
+            glPointSize(10.0);
+            glBegin(GL_POINTS);
+            glVertex3f(MarkerCm.getX(), -MarkerCm.getY(), 0.0f); // origin of the line
+            glEnd( );
+
+            markerAnt.x = MarkerCm.getX();
+            markerAnt.y = MarkerCm.getY();
+
+            if (flagCalibration == true)
+                noExisteTag = false;
+
+            keybd_event(VK_SHIFT,0x10,0 , 0); //Simulamos que se presiona tecla shift para evitar apagado de pantalla
+            keybd_event(VK_SHIFT,0x10, KEYEVENTF_KEYUP,0);
+        }
+        else if(flagCalibration == true){
+
+            noExisteTag = true;
+
+        }
+
+        //Si se está calibrando marcar el área
+        if (flagCalibration == true){
+
+            glLoadIdentity();
+
+            glColor3f(1,1.0,0.0);
+            glPointSize(10.0);
+
+            if (calibP3d.size() == 0){      //Si se realiza la primera captura
+
+                glBegin(GL_LINES);
+                glVertex3f(-0.5, 0.2, 0.0f);
+                glVertex3f(-1.0, 0.2, 0.0f);
+                glEnd( );
+
+                glBegin(GL_LINES);
+                glVertex3f(-0.5, 1.0, 0.0f);
+                ///glVertex3f(-0.5, 0.7, 0.0f);
+                glVertex3f(-0.5, 0.2, 0.0f);
+                glEnd( );
+            }
+
+            else if (calibP3d.size() == 1){    //Si se realiza la segunda captura
+
+                glBegin(GL_LINES);
+                glVertex3f(-0.5, -0.2, 0.0f);
+                glVertex3f(-1.0, -0.2, 0.0f);
+                glEnd( );
+
+                glBegin(GL_LINES);
+                glVertex3f(-0.5, -1.0, 0.0f);
+                glVertex3f(-0.5, -0.2, 0.0f);
+                glEnd( );
+            }
+
+            else if (calibP3d.size() == 2){    //Si se realiza la tercera captura
+
+                glBegin(GL_LINES);
+                glVertex3f(0.4, -0.2, 0.0f);
+                glVertex3f(1.0, -0.2, 0.0f);
+                glEnd( );
+
+                glBegin(GL_LINES);
+                glVertex3f(0.4, -0.2, 0.0f);
+                glVertex3f(0.4, -1.0, 0.0f);
+                glEnd( );
+            }
+
+            else if (calibP3d.size() == 3){    //Si se realiza la cuarta captura
+
+                glBegin(GL_LINES);
+                glVertex3f(0.4, 0.2, 0.0f);
+                glVertex3f(1.0, 0.2, 0.0f);
+                glEnd( );
+
+                glBegin(GL_LINES);
+                glVertex3f(0.4, 1.0, 0.0f);
+                glVertex3f(0.4, 0.2, 0.0f);
+                glEnd( );
+            }
+        }
+
+    }
+    // HANDLE WINDOW RESIZING
+     void CamWindow::resize(int x, int y, int w,int h) {
+
+        Fl_Gl_Window::resize(x,y,w,h);
+        TheGlWindowSize=Size(w,h);
+
+        if (w*3%4!=0) {
+            w+=w*3%4;//resize to avoid padding
+            resize(x,y,w,TheGlWindowSize.height);
+        }
+        else {
+
+            if (TheUndInputImage.rows!=0)
+                cv::resize(TheUndInputImage,TheResizedImage,TheGlWindowSize);
+        }
+
+    }
+
+    void CamWindow::timerCB(void *userdata) {
+        CamWindow *pb = (CamWindow*)userdata;
+        pb->redraw();
+        Fl::repeat_timeout(1.0/5000.0, timerCB, userdata);
+    }
+
 
         //Si se han encontrado tags
         if(TheMarkers.size() != 0){
