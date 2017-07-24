8f5797db2f34f4bef71ce118f90c243b527c3621
Emilio López <elp10.el@gmail.com>
2017-07-24 22:37:51 +0200

    Update CamWindow.hpp

--- EmilioLópez-ArucoTest/CamWindow.hpp
+++ EmilioLópez-ArucoTest/CamWindow.hpp
@@ -2,8 +2,6 @@
 #define _CAMWINDOW_H_
 #include <FL/Fl.H>
 #include <FL/Fl_Gl_Window.H>
-#include <iostream>
-#include <cstdlib>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
@@ -12,41 +10,53 @@
 #include <FL/fl_ask.H>
 #include "Point3D.hpp"
 
-#define NORMAL      0
-#define ENDOTROPIA  1
-#define EXOTROPIA   2
-#define HIPERTROPIA 3
-#define HIPOTROPIA  4
-
 using namespace aruco_test;
 using namespace aruco;
-using namespace std;
 using namespace cv;
 
-extern bool TheCaptureFlag, flagCalibration, noExisteTag, flagCalibration, noExisteTag;
-extern VideoCapture TheVideoCapturer;
-extern Mat TheInputImage, TheUndInputImage, TheResizedImage;
-extern MarkerDetector PPDetector;
-extern vector<Marker> TheMarkers;
-extern char cadena[200], buffer[200];
-extern Size TheGlWindowSize;
-extern Marker Marker1;
+extern bool flagCalibration, noExisteTag;
+extern VideoCapture videoCapturer;
+extern Mat inputImage;
+extern Marker markerChoosed;
 extern int econt;
-extern int curr_string_iz, curr_string_de, angle_iz, angle_de;
-extern CameraParameters TheCameraParams;
-extern Point3D MarkerPx, MarkerCm;
-extern float rotx1, roty1, rotz1, rotz2, rotx2, roty2, rotx, roty, rotz, TheMarkerSize;
-extern Point2f MarkerAnt;
-extern vector < vector<cv::Point3f>  > calib_p3d;
+extern CameraParameters cameraParams;
+extern Point2f markerAnt;
+extern vector < vector<cv::Point3f>  > calibP3d;
 
 namespace aruco_test{
 
     class CamWindow : public Fl_Gl_Window{
 
+        private:
         double fg;                       //brillo del primer plano
         double bg;                       //brillo de fondo
+        vector<Marker> TheMarkers;
+        Mat TheUndInputImage, TheResizedImage;
+        MarkerDetector PPDetector;
+        Size TheGlWindowSize;
 
         // OPENGL WINDOW CONSTRUCTOR
+        public:
+        CamWindow(int X,int Y,int W,int H,const char*L=0) : Fl_Gl_Window(X,Y,W,H,L) {
+        fg = 1.0;
+        bg = 0.0;
+        Fl::add_timeout(1.0/5000.0, timerCB, (void*)this);       // 24fps timer
+        end();
+    }
+    void SetBrightness(double new_fg, double new_bg)
+        { fg = new_fg; bg = new_bg; redraw(); }
+
+        void draw();
+        void resize(int x, int y, int w,int h);
+        static void timerCB(void *userdata);
+        void vIdle();
+        void detectMarker();
+
+    };
+
+}
+#endif
+
         public:
     CamWindow(int X,int Y,int W,int H,const char*L=0) : Fl_Gl_Window(X,Y,W,H,L) {
         fg = 1.0;
